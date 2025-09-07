7c695a0498ff0b7fac87cbadb5d73f487b41eb57
Gearhounds9242 <ycds9242@gmail.com>
2025-09-07 13:27:16 -0400

    Added the gradle files and made the mechanmun file with other subfolders

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/HackinHoundsHardware.java
@@ -0,0 +1,148 @@
+package org.firstinspires.ftc.teamcode.Hardware;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.hardware.dfrobot.HuskyLens;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
+import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
+import com.qualcomm.hardware.rev.Rev9AxisImuOrientationOnRobot;
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.hardware.AnalogSensor;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+import org.openftc.easyopencv.OpenCvCamera;
+
+// Generic robot class
+public class HackinHoundsHardware extends Hardware {
+    public HardwareMap robotMap;
+
+    // Drivetrain Members
+    public DcMotorEx  leftFront;
+    public DcMotorEx  rightFront;
+    public DcMotorEx  leftBack;
+    public DcMotorEx  rightBack;
+
+
+    public double lastAngle;
+
+    public IMU imu;
+    public YawPitchRollAngles angles;
+
+    private YawPitchRollAngles lastAngles;
+    private double globalAngle;
+
+    // 1000 ticks was roughly 18 in.
+    public static final double TICK_PER_INCH = 432/24;
+    public static final double MinPower = 0.1;
+
+
+
+    /**
+     GLOBAL VARIABLES
+     **/
+
+
+    /* Constructor */
+    public HackinHoundsHardware(){
+
+    }
+
+    // Override to set actual robot configuration
+    public void init(HardwareMap hwMap) {
+
+        // Save reference to Hardware map
+        robotMap = hwMap;
+
+
+        // Define and Initialize Motors for drivetrain
+        leftFront  = robotMap.get(DcMotorEx.class, "left_front");
+        rightFront = robotMap.get(DcMotorEx.class, "right_front");
+        leftFront.setDirection(DcMotorSimple.Direction.FORWARD);
+        rightFront.setDirection(DcMotorSimple.Direction.FORWARD);
+        leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        //leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        //rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBack = robotMap.get(DcMotorEx.class, "left_back");
+        rightBack = robotMap.get(DcMotorEx.class, "right_back");
+        rightBack.setDirection(DcMotorSimple.Direction.FORWARD);
+        leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftBack.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
+
+        //leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        //rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
+
+
+        // Defines the REV Hub's internal IMU (Gyro)
+        imu = robotMap.get(IMU.class, "imu");
+
+        //imu.resetYaw();
+
+        Rev9AxisImuOrientationOnRobot.I2cPortFacingDirection usb = Rev9AxisImuOrientationOnRobot.I2cPortFacingDirection.RIGHT;
+        Rev9AxisImuOrientationOnRobot.LogoFacingDirection logo = Rev9AxisImuOrientationOnRobot.LogoFacingDirection.DOWN;
+
+//        RevHubOrientationOnRobot.LogoFacingDirection logo = RevHubOrientationOnRobot.LogoFacingDirection.DOWN;
+//        RevHubOrientationOnRobot.UsbFacingDirection usb = RevHubOrientationOnRobot.UsbFacingDirection.LEFT;
+//        RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(logo, usb);
+        Rev9AxisImuOrientationOnRobot orientationOnRobot = new Rev9AxisImuOrientationOnRobot(logo, usb);
+        imu.initialize(new IMU.Parameters(orientationOnRobot));
+        imu.resetYaw();
+        lastAngle = 0;
+                //imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
+    }
+
+    public double clamp(double x, double min, double max) {
+        return Math.max(min,Math.min(max,x));
+    }
+
+    //
+    public double getAngle() {
+        double angle = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
+
+        double deltaAngle = angle - lastAngle;
+
+        if (deltaAngle < -180)
+            deltaAngle += 360;
+        else if (deltaAngle > 180)
+            deltaAngle -= 360;
+
+        globalAngle += deltaAngle;
+
+        lastAngle = angle;
+
+        return globalAngle;
+    }
+
+    public double getDistance (AnalogInput sensor) {
+        return (sensor.getVoltage() * 48.7) - 4.9;
+    }
+}
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/HackinHounds_Mechanum.java
@@ -0,0 +1,172 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import android.util.Size;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLStatus;
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.Hardware.HackinHoundsHardware;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.opencv.ColorBlobLocatorProcessor;
+import org.firstinspires.ftc.vision.opencv.ColorRange;
+import org.firstinspires.ftc.vision.opencv.ImageRegion;
+import org.opencv.core.RotatedRect;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+/*
+ * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+ * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+ * of the FTC Driver Station. When a selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all linear OpModes contain.
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
+ */
+
+@TeleOp(name="Mechanum", group="Linear OpMode")
+public class HackinHounds_Mechanum extends LinearOpMode {
+    // Declare OpMode members. aamir dont screw stuff up
+    private ElapsedTime runtime = new ElapsedTime();
+    private HackinHoundsHardware robot = new HackinHoundsHardware();
+
+
+    double shift = 1;
+
+
+
+    double cycleStart = 0;
+
+
+
+    @Override
+    public void runOpMode() {
+        robot.init(hardwareMap);
+
+
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.setMsTransmissionInterval(50);   // Speed up telemetry updates, Just use for debugging.
+        telemetry.setDisplayFormat(Telemetry.DisplayFormat.MONOSPACE);
+        telemetry.update();
+
+
+
+        waitForStart();
+        runtime.reset();
+
+
+
+
+        while (opModeIsActive()) {
+            cycleStart = runtime.milliseconds();
+            //telemetry.addData("Begin Time", "%f", runtime.milliseconds() - cycleStart);
+
+            if (gamepad1.dpad_up) {
+                shift = 1;
+            } else if (gamepad1.dpad_right) {
+                shift = 0.75;
+            } else if (gamepad1.dpad_down) {
+                shift = 0.5;
+            }
+            if (gamepad1.left_stick_button) {
+                shift = 1;
+            }
+
+
+
+
+            // Send data to Dashboard
+            TelemetryPacket packet = new TelemetryPacket();
+
+
+            /** DRIVING - Perfectly FINE (don't change) **/
+
+            //telemetry.addData("begining if's finished", "%f", runtime.milliseconds() - cycleStart);
+
+            double facing = robot.imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+            double y = -gamepad1.left_stick_y;
+            //double y = 0;
+            double x = gamepad1.left_stick_x;
+            double rx = gamepad1.right_stick_x;
+
+            double rotX = x * Math.cos(-facing) - y * Math.sin(-facing);
+            rotX = rotX * 1.1;
+            double rotY = x * Math.sin(-facing) + y * Math.cos(-facing);
+
+            double d = Math.max(Math.abs(rotX) + Math.abs(rotY) + Math.abs(rx), 1);
+
+            double lf = (rotY + rotX + rx) / d;
+            double lb = (rotY - rotX + rx) / d;
+            double rf = (rotY - rotX - rx) / d;
+            double rb = (rotY + rotX - rx) / d;
+
+            robot.leftBack.setVelocity(3000 * lb * shift);
+            robot.leftFront.setVelocity(3000 * lf * shift);
+            robot.rightBack.setVelocity(3000 * rb * shift);
+            robot.rightFront.setVelocity(3000 * rf * shift);
+
+            //telemetry.addData("Driving Finished", "%f", runtime.milliseconds() - cycleStart);
+
+            telemetry.addData("Telemtry finished", "%f", runtime.milliseconds() - cycleStart);
+            telemetry.update();
+        }
+    }
+
+}
+
