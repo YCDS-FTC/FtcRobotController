5a1fb31921b1a0c930ee176fef963b3f7b2ee10b
Gearhounds9242 <ycds9242@gmail.com>
2026-01-27 23:20:53 -0500

    fixed turret. No fail safes yet and turret has to end at 0 degrees for auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewAutosFixedPinpoint/Ball_LEBRON.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.NewAutosFixedPinpoint;
+
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.util.Timer;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.Hardware.HackinHoundsHardware;
+
+@Autonomous(name="18ball? LEBRONNNN", group = "examples")
+public class Ball_LEBRON {
+    
+    private HackinHoundsHardware robot = new HackinHoundsHardware();
+
+    private Follower follower;
+    private int pathState;
+    private Timer pathTimer, opmodeTimer;
+
+    private final Pose
+    
+}
